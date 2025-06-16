12b05eec8565a93fcb7c94d567fbc8785e7c7097
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2025-06-16 21:45:31 +0600

    (FE):  Imported mantine's own css

--- teebay-frontend/src/main.tsx
+++ teebay-frontend/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'  
 import { BrowserRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css';
 
 const client = new ApolloClient({
   uri: "https://localhost:3000/graphql",
@@ -15,7 +16,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <MantineProvider withGlobalStyles withNormalizeCSS>
+        <MantineProvider>
           <App />
         </MantineProvider>
       </BrowserRouter>
