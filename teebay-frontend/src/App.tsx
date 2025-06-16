dfb40d8c838f4efa9c724edbe86aacb14968951b
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2025-06-16 15:00:08 +0600

    (FE): basic landing page

--- teebay-frontend/src/App.tsx
+++ teebay-frontend/src/App.tsx
@@ -1,18 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-// import RegisterPage from './pages/RegisterPage';
-import ProductListPage from './pages/ProductListPage';
-// import AddProductPage from './pages/AddProductPage';
-// import TransactionsPage from './pages/TransactionsPage';
+import LandingPage from './pages/LandingPage';
+import './App.css'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<ProductListPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      {/* <Route path="/register" element={<RegisterPage />} />
-      <Route path="/add-product" element={<AddProductPage />} />
-      <Route path="/transactions" element={<TransactionsPage />} /> */}
+      <Route path="/" element={<LandingPage />} />
     </Routes>
   );
 }
---
+++ teebay-frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import '../styles/LandingPage.css';
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to TeeBay</h1>
+      <p>Your one-stop shop for buying and selling t-shirts.</p>
+      <p>Explore our products, manage your transactions, and enjoy a seamless experience.</p>
+      <p>Login or register to get started!</p>
+    </div>
+  );
+}
